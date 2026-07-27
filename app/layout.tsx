356aa5dbcb208bb01d8cdb65978079dfdb5ccbc0
mgia007 <georgia.mihalca@mtdtechnology.net>
2026-07-27 16:33:09 +0300

    font fix up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,42 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Pinyon_Script, Didact_Gothic } from "next/font/google";
-import { Playfair_Display } from 'next/font/google'
-import { Comfortaa } from 'next/font/google'
+import { Pinyon_Script, Comfortaa } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 const pinyonScript = Pinyon_Script({
-  weight: '400',
-  subsets: ['latin'],
-  variable: '--font-pinyon',
-});
-
-const didactGothic = Didact_Gothic({
-  weight: '400', 
-  subsets: ['latin'],
-  variable: '--font-didact',
-});
-
-const perandoryFont = Playfair_Display({
-  subsets: ['latin'],
-  variable: '--font-perandory',
-});
-
-const comfortaaFont = Comfortaa({
-  subsets: ['latin'],
-  weight: ['500'], 
-  variable: '--font-comfortaa',
-});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+  weight: "400",
   subsets: ["latin"],
+  variable: "--font-pinyon",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const comfortaa = Comfortaa({
+  weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
+  variable: "--font-comfortaa",
+});
+
+const interDisplay = localFont({
+  src: [
+    { path: "./fonts/Font - Inter Display/InterDisplay-Regular.ttf", weight: "400" },
+    { path: "./fonts/Font - Inter Display/InterDisplay-Medium.ttf", weight: "500" },
+    { path: "./fonts/Font - Inter Display/InterDisplay-SemiBold.ttf", weight: "600" },
+    { path: "./fonts/Font - Inter Display/InterDisplay-Bold.ttf", weight: "700" },
+  ],
+  variable: "--font-inter-display",
+});
+
+const futuraPt = localFont({
+  src: [
+    { path: "./fonts/futura-pt/FuturaCyrillicBook.ttf", weight: "400" },
+    { path: "./fonts/futura-pt/FuturaCyrillicMedium.ttf", weight: "500" },
+    { path: "./fonts/futura-pt/FuturaCyrillicBold.ttf", weight: "700" },
+  ],
+  variable: "--font-futura",
+});
+
+const perandory = localFont({
+  src: "./fonts/PerandorySemiCondensed.otf",
+  variable: "--font-perandory",
 });
 
 export const metadata: Metadata = {
@@ -50,7 +52,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} ${pinyonScript.variable} ${didactGothic.variable} ${perandoryFont.variable} ${comfortaaFont.variable} h-full antialiased`}
+      className={` ${pinyonScript.variable} ${comfortaa.variable} ${perandory.variable} ${futuraPt.variable} ${interDisplay.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-black text-white">
         {children}
