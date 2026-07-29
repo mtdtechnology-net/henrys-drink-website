fcaac418e83be68d8240877800849d4c5f4b794e
mgia007 <georgia.mihalca@mtdtechnology.net>
2026-07-29 11:04:55 +0300

    nightlife page-in review

--- app/[locale]/nightlife/page.tsx
+++ app/[locale]/nightlife/page.tsx
@@ -7,6 +7,7 @@ import Section5 from "@/components/nightlife/section5";
 
 export default function NightlifePage() {
   return (
+    
     <main className="h-screen w-full bg-black overflow-y-scroll text-white relative">
       <Section1 />
       <Section2 />
