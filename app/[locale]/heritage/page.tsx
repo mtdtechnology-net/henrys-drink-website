d2c3d22a4f435536a04f910fefed7fdd1295e83d
giulisem <giulia.semeniuc@mtdtechnology.net>
2026-07-29 13:35:17 +0300

    footer done with light and dark theme

--- app/[locale]/heritage/page.tsx
+++ app/[locale]/heritage/page.tsx
@@ -3,17 +3,6 @@ import { Footer } from "@/components/footer/Footer";
 export default function HeritagePage() {
   return (
     <main className="min-h-screen w-full bg-[url('/image.svg')] bg-cover bg-center bg-no-repeat text-white">
-      <h1 className="p-8 text-3xl font-bold">My Heritage Page</h1>
-      <Footer
-  theme="dark"
-  privacyHref="/privacy-policy"
-  shippingHref="/shipping-delivery"
-/>
-<Footer
-  theme="light"
-  privacyHref="/privacy-policy"
-  shippingHref="/shipping-delivery"
-/>
     </main>
     
   );
