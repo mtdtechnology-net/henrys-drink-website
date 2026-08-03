c210a2c821b53b37c8fd8e0034cad669c3684237
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2026-08-03 12:54:14 +0300

    fixed the design heritage page

--- components/heritage/FamilyStorySection.tsx
+++ components/heritage/FamilyStorySection.tsx
@@ -9,15 +9,16 @@ export function FamilyStorySection() {
     >
       <div className="absolute inset-0 z-0 bg-[url('/vines.svg')] bg-cover bg-center bg-no-repeat grayscale after:absolute after:inset-0 after:bg-black/[0.06] after:content-['']" />
 
-      <div className="absolute left-[18.594%] top-[min(18.698vw,359px)] z-[3] aspect-[362/486] w-[18.854%] max-w-[362px] [filter:drop-shadow(0_4px_15.7px_rgba(0,0,0,0.35))] max-[768px]:relative max-[768px]:left-auto max-[768px]:top-auto max-[768px]:mb-14 max-[768px]:w-[210px] max-[768px]:shrink-0 max-[768px]:self-start max-[768px]:translate-x-[8vw] max-[480px]:mb-12 max-[480px]:w-[170px] max-[480px]:translate-x-[5vw]">
+      <div className="relative z-[2] mx-auto min-h-[max(100svh,min(56.25vw,1080px))] w-full max-w-[1920px] max-[768px]:flex max-[768px]:min-h-0 max-[768px]:flex-col max-[768px]:items-center">
         <Image
-          className="object-contain"
+          className="absolute left-[18.594%] top-[min(18.698vw,359px)] z-[3] h-auto w-[18.854%] max-w-[362px] object-contain [filter:drop-shadow(0_4px_15.7px_rgba(0,0,0,0.35))] max-[768px]:relative max-[768px]:left-auto max-[768px]:top-auto max-[768px]:mb-14 max-[768px]:w-[210px] max-[768px]:shrink-0 max-[768px]:self-start max-[768px]:translate-x-[8vw] max-[480px]:mb-12 max-[480px]:w-[170px] max-[480px]:translate-x-[5vw]"
           src="/logo-burgundy.svg"
           alt=""
-          fill
-          sizes="(max-width: 768px) 210px, 18.854vw"
+          width={362}
+          height={486}
           aria-hidden="true"
         />
+
         <article className="absolute left-[55.99%] top-[min(10.885vw,209px)] z-[2] box-border flex h-[min(40.938vw,786px)] w-[29.427%] max-w-[565px] flex-col overflow-hidden bg-[#fffcf9] px-[min(2.604vw,50px)] pb-[min(2.604vw,50px)] pt-[min(2.604vw,50px)] text-[#442f0e] min-[1600px]:h-[740px] min-[1600px]:w-[535px] min-[1600px]:px-[45px] min-[1600px]:pb-[38px] min-[1600px]:pt-[38px] max-[768px]:relative max-[768px]:left-auto max-[768px]:top-auto max-[768px]:h-auto max-[768px]:w-full max-[768px]:max-w-[500px] max-[768px]:px-6 max-[768px]:pb-10 max-[768px]:pt-8 max-[480px]:px-5">
           {" "}
           <h2
