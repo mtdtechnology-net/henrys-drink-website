d9e20c3b5ed2589e137dc70ff62586b6b0735f40
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2026-08-03 12:50:29 +0300

    fixed the warnings of the images on heritage page

--- components/heritage/FamilyStorySection.tsx
+++ components/heritage/FamilyStorySection.tsx
@@ -9,23 +9,23 @@ export function FamilyStorySection() {
     >
       <div className="absolute inset-0 z-0 bg-[url('/vines.svg')] bg-cover bg-center bg-no-repeat grayscale after:absolute after:inset-0 after:bg-black/[0.06] after:content-['']" />
 
-      <div className="relative z-[2] mx-auto min-h-[max(100svh,min(56.25vw,1080px))] w-full max-w-[1920px] max-[768px]:flex max-[768px]:min-h-0 max-[768px]:flex-col max-[768px]:items-center">
+      <div className="absolute left-[18.594%] top-[min(18.698vw,359px)] z-[3] aspect-[362/486] w-[18.854%] max-w-[362px] [filter:drop-shadow(0_4px_15.7px_rgba(0,0,0,0.35))] max-[768px]:relative max-[768px]:left-auto max-[768px]:top-auto max-[768px]:mb-14 max-[768px]:w-[210px] max-[768px]:shrink-0 max-[768px]:self-start max-[768px]:translate-x-[8vw] max-[480px]:mb-12 max-[480px]:w-[170px] max-[480px]:translate-x-[5vw]">
         <Image
-          className="absolute left-[18.594%] top-[min(18.698vw,359px)] z-[3] h-auto w-[18.854%] max-w-[362px] object-contain [filter:drop-shadow(0_4px_15.7px_rgba(0,0,0,0.35))] max-[768px]:relative max-[768px]:left-auto max-[768px]:top-auto max-[768px]:mb-14 max-[768px]:w-[210px] max-[768px]:shrink-0 max-[768px]:self-start max-[768px]:translate-x-[8vw] max-[480px]:mb-12 max-[480px]:w-[170px] max-[480px]:translate-x-[5vw]"
+          className="object-contain"
           src="/logo-burgundy.svg"
           alt=""
-          width={362}
-          height={486}
+          fill
+          sizes="(max-width: 768px) 210px, 18.854vw"
           aria-hidden="true"
         />
-
-<article className="absolute left-[55.99%] top-[min(10.885vw,209px)] z-[2] box-border flex h-[min(40.938vw,786px)] w-[29.427%] max-w-[565px] flex-col overflow-hidden bg-[#fffcf9] px-[min(2.604vw,50px)] pb-[min(2.604vw,50px)] pt-[min(2.604vw,50px)] text-[#442f0e] min-[1600px]:h-[740px] min-[1600px]:w-[535px] min-[1600px]:px-[45px] min-[1600px]:pb-[38px] min-[1600px]:pt-[38px] max-[768px]:relative max-[768px]:left-auto max-[768px]:top-auto max-[768px]:h-auto max-[768px]:w-full max-[768px]:max-w-[500px] max-[768px]:px-6 max-[768px]:pb-10 max-[768px]:pt-8 max-[480px]:px-5">          <h2
+        <article className="absolute left-[55.99%] top-[min(10.885vw,209px)] z-[2] box-border flex h-[min(40.938vw,786px)] w-[29.427%] max-w-[565px] flex-col overflow-hidden bg-[#fffcf9] px-[min(2.604vw,50px)] pb-[min(2.604vw,50px)] pt-[min(2.604vw,50px)] text-[#442f0e] min-[1600px]:h-[740px] min-[1600px]:w-[535px] min-[1600px]:px-[45px] min-[1600px]:pb-[38px] min-[1600px]:pt-[38px] max-[768px]:relative max-[768px]:left-auto max-[768px]:top-auto max-[768px]:h-auto max-[768px]:w-full max-[768px]:max-w-[500px] max-[768px]:px-6 max-[768px]:pb-10 max-[768px]:pt-8 max-[480px]:px-5">
+          {" "}
+          <h2
             id="family-story-title"
             className="relative z-[3] mb-[min(1.042vw,20px)] mt-0 flex h-[min(3.542vw,68px)] w-full shrink-0 items-center justify-center whitespace-nowrap text-center align-middle font-['Perandory',Georgia,serif] text-[min(3.229vw,62px)] font-normal leading-none tracking-[0] text-[#442f0e] [font-stretch:semi-condensed] max-[768px]:mb-6 max-[768px]:h-auto max-[768px]:min-h-[52px] max-[768px]:whitespace-normal max-[768px]:text-[clamp(42px,10vw,56px)]"
           >
             A FAMILY STORY
           </h2>
-
           <div className="relative z-[2] aspect-[1.07/1] w-full shrink-0 overflow-hidden">
             <Image
               className="object-cover object-center grayscale"
@@ -35,7 +35,6 @@ export function FamilyStorySection() {
               sizes="(max-width: 768px) 85vw, 24.22vw"
             />
           </div>
-
           <div className="relative mt-[min(0.833vw,16px)] flex min-h-0 flex-1 items-start">
             <Image
               className="pointer-events-none absolute bottom-[-18%] left-[-10%] z-[1] h-auto w-[78%] object-contain opacity-65 [filter:saturate(1.5)_brightness(0.95)] max-[768px]:bottom-[-45px] max-[768px]:left-[-75px] max-[768px]:h-[270px] max-[768px]:w-[305px]"
--- components/heritage/FloatingBottle.tsx
+++ components/heritage/FloatingBottle.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export function FloatingBottle() {
   return (
     <div
-      className="pointer-events-none absolute bottom-0 right-[clamp(-20px,3vw,60px)] z-[20] w-[clamp(410px,33vw,680px)] origin-bottom scale-y-[1.10] [&_img]:block [&_img]:h-auto [&_img]:w-full max-[1200px]:right-[1%] max-[1200px]:w-[440px] max-[1024px]:right-[-3%] max-[1024px]:w-[380px] max-[768px]:right-[-14%] max-[768px]:w-[300px] max-[480px]:right-[-20%] max-[480px]:w-[245px] [@media(min-width:769px)_and_(max-height:1000px)]:!right-[2%] [@media(min-width:769px)_and_(max-height:1000px)]:!w-[410px] min-[1600px]:bottom-[70px] min-[1600px]:scale-y-[1.22] min-[1600px]:w-[clamp(460px,31vw,700px)]"
+      className="pointer-events-none absolute bottom-0 right-[clamp(-20px,3vw,60px)] z-[20] aspect-[410/1200] w-[clamp(410px,33vw,680px)] origin-bottom scale-y-[1.10] max-[1200px]:right-[1%] max-[1200px]:w-[440px] max-[1024px]:right-[-3%] max-[1024px]:w-[380px] max-[768px]:right-[-14%] max-[768px]:w-[300px] max-[480px]:right-[-20%] max-[480px]:w-[245px] [@media(min-width:769px)_and_(max-height:1000px)]:!right-[2%] [@media(min-width:769px)_and_(max-height:1000px)]:!w-[410px] min-[1600px]:bottom-[70px] min-[1600px]:scale-y-[1.22] min-[1600px]:w-[clamp(460px,31vw,700px)]"
       aria-hidden="true"
     >
       <Image
         src="/bottle.svg"
         alt=""
-        width={410}
-        height={1200}
+        fill
+        className="object-contain"
         sizes="(max-width: 768px) 48vw, 410px"
       />
     </div>
